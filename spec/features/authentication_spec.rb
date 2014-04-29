f59fadc782ef5d7d0e9120d4d734127f9d57ce7d
cj <cjschaff@gmail.com>
2014-04-29 01:04:25 +0000

    created and passed sign up scenario

--- spec/features/authentication_spec.rb
+++ spec/features/authentication_spec.rb
@@ -18,4 +18,24 @@ feature 'Authentication' do
     expect(page).to have_content "Sign in" #expect a welcome message
     
   end
+  
+   scenario "sign up" do
+      
+    visit root_path#visit homepath,visit root_path#visit homepath, 
+    
+    click_link "Sign in"
+     click_link "Sign up"
+     
+    fill_in "Email", with: "factorygirl@cjhosting.net"#fill in email, 
+    fill_in "Password", with: "password"#fill in a password,
+     
+    click_button "Sign up" #submit
+     
+    expect(page).to have_content "Welcome" #expect a welcome message
+    expect(page).to have_content "Sign out" #log out option
+    
+    click_link "Sign out"
+    expect(page).to have_content "Sign in" #expect a welcome message
+    
+  end
 end
